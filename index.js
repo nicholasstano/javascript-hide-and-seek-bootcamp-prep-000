4215c878e253335383f76c7024c5fcb954f19006
Nicholas Stano <nicholasstano@gmail.com>
2019-01-01 23:55:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
 
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() { 
+  let node = document.getElementById('grand-node');
+  let nextNode = node.children[0];
+  
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0];
+  }
+  return node;
+}
+
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list');
+    
+  for (let i = 0, j = rankedLists.length; i < j; i++) {
+    let children = rankedLists[i].children;
+  
+  for (let l = 0, k = children.length; l < k; l++) {
+    children[l].innerHTML = parseInt(children[l].innerHTML) + n;
+  }
+  }
+}
